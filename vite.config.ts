b6f2bb523e9b784c0327fd2fd785902f630953aa
Maxim Axenov <topcmc32@gmail.com>
2024-10-10 14:01:17 +0500

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mini/',
   plugins: [react()],
   server: { port: 3002 },
   resolve: {
